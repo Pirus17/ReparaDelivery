1c7c2ce792dc5066f0cdcf19217d00c5f7aa68e0
Pirus17 <mariohernaperalta.17@gmail.com>
2024-02-20 09:36:51 -0300

    funciona el menu de filtrar utilizando base de datos

--- model/productoModel.js
+++ model/productoModel.js
@@ -1,8 +1,15 @@
+const { query } = require("express");
+
 module.exports = {
     obtener: function (conexion, funcion) {
-
-        conexion.query("SELECT * FROM producto", funcion);
+    let marca="tcl";
+     conexion.query("SELECT * FROM producto WHERE Marca=?",[marca],funcion);
+    },
+    
+    porMarca: function (conexion, marca, funcion) {
+        console.log(marca);
+        conexion.query("SELECT * FROM producto WHERE Marca=?",[marca.marca],funcion);
+        
     }
-
 
 }
--- routes/producto.js
+++ routes/producto.js
@@ -5,7 +5,7 @@ const productoControllers= require('../controllers/productoControllers');
 /* GET home page (home page). */
 router.get('/', productoControllers.index);
 
-
+router.post('/', productoControllers.filtrar);
 
 
 module.exports = router;
