786d525d9860d372f1e985673e4f75599612688a
Pirus17 <mariohernanperalta.s17>
2024-02-24 02:21:29 -0300

    termine la barra de busqueda

--- controllers/busquedaControllers.js
+++ controllers/busquedaControllers.js
@@ -5,30 +5,25 @@ var marca, modelo;
 module.exports = {
 
     index: function (req, res) {
-        if (validarBusqueda(req.body)) {
-            producto.barraBusqueda(conexion, marca, modelo, function (err, datos) {
-
-                res.render('productos/index', { title: 'Productos', productos: datos });
-
-            });
-
-        }
-    }
-}
-
-function validarBusqueda(marcaModelo) {
-    if (elementoVacio(marcaModelo)) {
+        let marcaModelo= req.body;
         let dividir = marcaModelo.busqueda.split(" ");
         marca = dividir[0];
         modelo = dividir.slice(1).join(" ");
-        return true;
-    } else {
-        return false;
+        producto.barraBusqueda(conexion, marca, modelo, function (err, datos) {
+            console.log(datos);
+            if (datos.length>0) { 
+                res.render('productos/index', { title: 'Productos', productos: datos });
+            } else {
+                console.log("false");
+                res.render('busqueda/index', { title: 'Busqueda' });
+                
+            }
+
+
+        });
+
     }
-
 }
 
-function elementoVacio(marcaModelo) {
-    return (marcaModelo.busqueda && marcaModelo.busqueda.trim() !== '');
 
-}
+
