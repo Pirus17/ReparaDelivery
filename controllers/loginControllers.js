f44c907f0192ce5dd0cb13fca91d57028306bf67
Pirus17 <mariohernanperalta.17@gmail.com>
2024-02-25 11:49:47 -0300

    configurando el controller login

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -1,25 +1,20 @@
 var conexion = require('../config/conexion');
 var login = require('../model/loginModel');
 
+
 module.exports = {
 
-   index: function (req, res) {
-    
-    res.render('login/index', { login: 'Login'});
+    index: function (req, res) {
+
+        res.render('login/index', { login: 'Login' });
 
     },
 
-    datosUsuario: function(req,res){
-        login.buscarTecnico(conexion,req.body,function(err,datos){
-
-    });
-    if (datos.lenght>0) {
-        res.render('login/index', { login: 'Login', usuario:datos});
-    } else {
-        login.buscarTecnico(conexion,req.body,function(err,datos){
-
+    datosUsuario: function (req, res) {
+        login.buscarTecnico(conexion, req.body, function (err, datos) {
+          console.log(datos);  
+          res.render('login/index', { login: 'Login' });
         });
-    }
-
+       
     }
 }
--- model/loginModel.js
+++ model/loginModel.js
@@ -2,7 +2,7 @@ const { query } = require("express");
 
 module.exports = {
     buscarTecnico: function (conexion, email, funcion) {
-     conexion.query("SELECT * FROM tecnico WHERE Email= ?",[email.email], funcion);
+     conexion.query("SELECT * FROM tecnico  WHERE email=?",[email.email],  funcion);
     },  
     buscarCliente: function (conexion, email, funcion) {
         conexion.query("SELECT * FROM tecnico WHERE Email= ?",[email.email], funcion);
